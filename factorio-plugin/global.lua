5339ac2388edba98e4fec33d1661bbd391e788d7
Nicholas Dunn <nexela@gmail.com>
2022-07-23 22:47:36 -0400

    Fixes and Optimizations

--- factorio-plugin/global.lua
+++ factorio-plugin/global.lua
@@ -7,6 +7,8 @@ local global_name_builder = {
   "__",
   "FallbackModName",
   "__",
+  "global",
+  "__",
 }
 
 ---Rename `global` so we can tell them apart!
@@ -18,23 +20,24 @@ local function replace(uri, text, diffs, this_mod)
 
   local scenario = uri:match("scenarios[\\/]([^\\/]+)[\\/]")--[[@as string?]]
   global_name_builder[2] = this_mod or "FallbackModName"
-  global_name_builder[4] = scenario and scenario or nil
-  global_name_builder[5] = scenario and "__" or nil
+  global_name_builder[6] = scenario and scenario or nil
+  global_name_builder[7] = scenario and "__" or nil
 
-  -- build string and replace any special characters with _
-  local global_name = table.concat(global_name_builder, "")
-  global_name = global_name:gsub("[^a-zA-Z0-9_]","_")
-
+  ---Build the global name and replace any special characters with _
+  local global_name = table.concat(global_name_builder, ""):gsub("[^a-zA-Z0-9_]","_")
   local global_matches = {} ---@type integer[]
-  for start, finish in text:gmatch("%f[a-zA-Z0-9_]()global()%s*[=.%[]")--[[@as fun():integer, integer]] do
+
+  for start, finish in text:gmatch("%f[a-zA-Z0-9_]()global()%s*[=.%[]") --[[@as fun():integer, integer]] do
     global_matches[start] = finish
   end
-  -- remove matches that where `global` is actually indexing into something (`.global`)
-  for start in text:gmatch("%.[^%S\n]*()global%s*[=.%[]")--[[@as fun():integer]] do
+
+  ---Remove matches that were `global` indexing into something (`T.global`)
+  for start in text:gmatch("%.[^%S\n]*()global%s*[=.%[]") --[[@as fun():integer]] do
     global_matches[start] = nil
   end
+
   -- `_ENV.global` and `_G.global` now get removed because of this, we can add them back in
-  -- with the code bellow, but it's a 66% performance cost increase for hardly any gain
+  -- with the code below, but it's a 66% performance cost increase for hardly any gain
   -- for start, finish in text:gmatch("_ENV%.%s*()global()%s*[=.%[]") do
   --   global_matches[start] = finish
   -- end
@@ -46,7 +49,7 @@ local function replace(uri, text, diffs, this_mod)
     util.add_diff(diffs, start, finish, global_name)
   end
 
-  -- and "define" it at the start of any file that used it
+  --- and "define" it at the start of any file that used it
   if next(global_matches) then
     util.add_diff(diffs, 1, 1, global_name.."={}---@diagnostic disable-line:lowercase-global\n")
   end
--- plugin.lua
+++ plugin.lua
@@ -7,7 +7,9 @@
 
 ---Dev Notes: confirm "path/to/lua-language-server/script/", in Lua.Workspace.Library for completions
 
+local ws = require("workspace")
 local scp = require("workspace.scope")
+local cfg = require('config')
 
 local require_module = require("factorio-plugin.require")
 local global = require("factorio-plugin.global")
@@ -17,62 +19,73 @@ local on_event = require("factorio-plugin.on-event")
 ---@alias fplugin.modes 'folder' | 'mods'
 ---@class fplugin.settings
 ---@field mode fplugin.modes
----@field mod_name? string
----@field mods_root? string
+---@field mod_name? string Cached value of the mod name, not used if mode is `mods`
+---@field mods_root? string Cached value of the mods root folder, not used if mode is `folder`
 local settings = {
   mode = "folder",
-  fallback_mod_name = "FallbackModName"
+  fallback_mod_name = "FallbackModName",
+  ---pluginArgs can be retrieve from the config object or as the second vararg of this chunk.
+  plugin_args = cfg.get(ws.rootUri, "Lua.runtime.pluginArgs") ---@type string[]
 }
 
 do ---@block Settings
-  local args = select(2, ...) ---@type string[]
+  local args = settings.plugin_args
   for i = 1, #args do
-    --mode: Switch between folder mode where each mod is its own workspace, and mods mode where the root mods folder is the workspace.
+    ---`--mode` can be either
+    ---`folder` where each mod is its own workspace, or
+    ---`mods` where the root mods folder is the workspace.
     local setting, mode = args[i]:match("^%-%-(%w+)%s*(%w+)") ---@type string, string
     if setting == "mode" then
       if not (mode == "folder" or mode == "mods") then
-        return log.error("wrong mode for plugin: ".. mode .. " expected 'mods' or 'folder'.")
+        return log.error("wrong mode for plugin: " .. mode .. " expected 'mods' or 'folder'.")
       end
       settings.mode = mode
       log.info(("Plugin running in %s mode"):format(mode))
     end
   end
 end
-print("Factorio Plugin loaded in ".. settings.mode .." mode")
+print("Factorio Plugin loaded in " .. settings.mode .. " mode")
 
-local function get_mod_name(uri)
-  if settings.mod_name then return settings.mod_name end
+  --[[
+    In `folder` mode cache and return the mod name.\
+    In `mods` mode the mod name changes depending on the file being edited so it can't be cached in `settings.mod_name`.\
+    For example in the file `C:\\Factorio-Game\\MyModsFolder\\MyMod\\control.lua`
+    The mod_root would be `MyModsFolder` which can be cached as `settings.mod_root` and `MyMod` would be the mod_name which can't be cached.
+  ]]
+---@param scope scope
+---@param file_uri string
+local function get_mod_name(scope, file_uri)
+  -- if settings.mod_name then return settings.mod_name end
 
+  local mode = settings.mode
   local mod_name ---@type string?
 
   ---Cache the mod name in folder mode
-  if settings.mode == "folder" then
-    mod_name = scp.getScope(uri).uri:match("[^/\\]+$")  --[[@as string?]]
+  if mode == "folder" then
+    mod_name = scope.uri:match("[^/\\]+$") --[[@as string?]]
     settings.mod_name = mod_name
     return mod_name
   end
 
-  ---Mods mode the mod name changes depending on the file being edited
-  if settings.mode == "mods" then
-    -- The root folder path
+  if mode == "mods" then
+    -- Cache The root folder path
     local root = settings.mods_root
     if not root then
-      local scope = scp.getScope(uri)
       -- get the end folder of root path
       root = scope.uri:match("[^/\\]+$")
       settings.mods_root = root
     end
 
-    -- get the first folder after root path
-    mod_name = uri:match(root .."[\\/]([^\\/]+)[\\/]") --[[@as string?]]
+    -- Get the first folder after root path
+    mod_name = file_uri:match(root .. "[\\/]([^\\/]+)[\\/]") --[[@as string?]]
     if not mod_name then
-      log.warn(("Could not determine mod name for uri: %s in %s mode."):format(uri, settings.mode))
+      log.warn(("Could not determine mod name for uri: %s in %s mode."):format(file_uri, mode))
       mod_name = settings.fallback_mod_name
     end
-    print("in mod name ", mod_name)
     return mod_name
   end
 
+  ---Shouldn't be possible to here here, but just in case.
   return settings.fallback_mod_name
 end
 
@@ -86,14 +99,14 @@ end
 ---@param text string @ The content of file
 ---@return nil|Diff[]
 function OnSetText(uri, text)
-  if scp.getScope(uri):isLinkedUri(uri) then return end
-
+  local scope = scp.getScope(uri)
+  if scope:isLinkedUri(uri) then return end
   if text:sub(1, 8) == "---@meta" or text:sub(1, 4) == "--##" then return end
 
-  local diffs = {count = 0} ---@type Diff.ArrayWithCount
+  local diffs = { count = 0 } ---@type Diff.ArrayWithCount
 
   require_module.replace(uri, text, diffs)
-  global.replace(uri, text, diffs, get_mod_name(uri))
+  global.replace(uri, text, diffs, get_mod_name(scope, uri))
   remote.replace(uri, text, diffs)
   on_event.replace(uri, text, diffs)
 
